fa81ad2153c889fee18c02a21c2226cf6225a0d7
murat <murat.byazrov@yandex.ru>
2022-06-01 01:24:12 +0300

    refactoring chats store

--- src/features/chats/chatsSlice.js
+++ src/features/chats/chatsSlice.js
@@ -1,5 +1,4 @@
 import {createSlice} from '@reduxjs/toolkit';
-import {servers} from '../../servers'
 
 export const chatsSlice = createSlice({
     name: 'chats',
@@ -9,22 +8,9 @@ export const chatsSlice = createSlice({
     },
     reducers: {
         addChats: (state, action) => {
-
         },
-        setChats: async state => {
-            const chats = await servers.messenger.getChats({
-                limit: 100,
-                userId: '5',
-            });
-
-            for (const chat of chats) {
-                chat.messages = await servers.messenger.getMessages({
-                    limit: 1,
-                    chatId: chat.chatId
-                })
-            }
-
-            chats && (state.list = chats);
+        setChats: (state, action) => {
+            state.list = [...action.payload];
         },
         removeChat: (state, action) => {
             state.list = state.list.filter(({chatId}) => chatId !== action.payload);
